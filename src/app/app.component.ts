ee9ee33e936386ec6aab5c2d050701f740ac5783
wojtur <mackowski28@gmail.com>
2024-10-04 12:43:34 +0200

    Created responsive navbar and other stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatButton} from '@angular/material/button';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatButton, MatMenu, MatMenuItem, MatMenuTrigger, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
