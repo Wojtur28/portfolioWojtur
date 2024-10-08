27cd4f650aa721458a4a63bd806333bcd25980cd
wojtur <mackowski28@gmail.com>
2024-10-08 09:33:17 +0200

    Basic architecture and images

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  logos = [
+    {src: 'assets/images/java.png', alt: 'Java logo'},
+    {src: 'assets/images/springboot.png', alt: 'Springboot logo'},
+    {src: 'assets/images/postgres.png', alt: 'Postgresql logo'}
+  ]
 
 }
