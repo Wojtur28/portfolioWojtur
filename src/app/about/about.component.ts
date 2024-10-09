9a6e55d84397e85f707114924f7fd6a6fa643017
wojtur <mackowski28@gmail.com>
2024-10-09 14:07:27 +0200

    Fix css in about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,7 +15,10 @@ export class AboutComponent {
   logos = [
     {src: 'assets/images/java.png', alt: 'Java logo'},
     {src: 'assets/images/springboot.png', alt: 'Springboot logo'},
-    {src: 'assets/images/postgres.png', alt: 'Postgresql logo'}
+    {src: 'assets/images/postgres.png', alt: 'Postgresql logo'},
+    {src: 'assets/images/angular.png', alt: 'Angular logo'},
+    {src: 'assets/images/docker.png', alt: 'Docker logo'},
+    {src: 'assets/images/githubaction.png', alt: 'Github action logo'},
   ]
 
 }
